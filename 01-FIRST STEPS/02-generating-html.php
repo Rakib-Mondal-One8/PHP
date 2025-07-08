8450fbf8889559be25682ed74f0e93385cf841e1
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2025-07-08 22:40:04 +0530

    Section 2 | Upto lecture 17.

--- 01-FIRST STEPS/02-generating-html.php
+++ 01-FIRST STEPS/02-generating-html.php
@@ -6,8 +6,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        .body-1 {
+            background-color: darkolivegreen;
+            color: aquamarine;
+        }
+        .body-2 {
+            background-color: yellowgreen;
+            color: aquamarine;
+        }
+        .body-3 {
+            background-color: cadetblue;
+            color: aquamarine;
+        }
+        .body-4 {
+            background-color: darkred;
+            color: aquamarine;
+        }
+        .body-5 {
+            background-color: azure;
+            color: aquamarine;
+        }
+        .body-6 {
+            background-color: yellow;
+            color: aquamarine;
+        }
+    </style>
 </head>
-<body>
+<body class="body-<?php echo rand(1,6); ?>">
     <?php echo "<h1>Hello World!</h1>"; ?>
 </body>
 </html>
---
+++ 01-FIRST STEPS/04-variables/variables.php
@@ -0,0 +1,12 @@
+<?php
+
+$greeting = 'Hello Php ! . I am Rakib Mondal';
+$year = 2050;
+
+echo $greeting;
+echo $greeting;
+echo $greeting;
+echo $greeting;
+echo $year;
+
+?>
