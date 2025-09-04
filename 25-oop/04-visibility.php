f49fb48085f39f6e783a693756e0f58b78e60172
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2025-09-04 14:31:00 +0530

    Upto Lecture - 265

---
+++ 25-oop/04-visibility.php
@@ -0,0 +1,49 @@
+<?php
+
+header('Content-Type: text/plain');
+
+class Animal
+{
+    public function __construct(private int $weight) {}
+    public function move()
+    {
+        echo "Animal::move() has been called\n";
+    }
+
+    public function eat()
+    {
+        echo "Animal::eat() has been called ({$this->weight})\n";
+    }
+}
+
+
+class Dog extends Animal
+{
+    /*
+    public function __construct() {
+        // $this->weight = 30;
+        parent::__construct(40);
+    }
+        */
+    public int $weight;
+    public function __construct(public string $breed, $weight)
+    {
+        $this->weight = 500;
+        parent::__construct($weight);
+    }
+    public function bark()
+    {
+        echo "Dog::bark() has been called (breed: {$this->breed}, weight: {$this->weight})\n";
+    }
+    public function move()
+    {
+        echo "Dog::move() has been called\n";
+    }
+}
+
+$dog = new Dog('Golden Retriever', 25);
+// $dog->weight = 40000;
+// var_dump($dog);
+$dog->move();
+$dog->bark();
+$dog->eat();
