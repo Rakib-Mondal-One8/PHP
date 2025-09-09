4cf2be2c7adc2dcb4375495b0ffa8a1de392e0d4
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2025-09-09 13:28:56 +0530

    Section - 30 | Done

---
+++ 28-cms/00 - part 00/index.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Frontend\Controller\NotFoundController;
+use App\Frontend\Controller\PagesController;
+use App\Repository\PagesRepository;
+
+require __DIR__ . '/inc/all.inc.php';
+
+$route = @(string) ($_GET['route'] ?? 'pages');
+
+if($route === 'pages'){
+
+    $page = @(string) ($_GET['page'] ?? 'index');
+
+    $pagesRepository = new PagesRepository($pdo);
+
+    $pagesController = new PagesController($pagesRepository);
+    $pagesController->showPage($page);
+}
+else{
+    $pagesRepository = new PagesRepository($pdo);
+
+    $notFoundController = new NotFoundController($pagesRepository);
+    $notFoundController->error404();
+}   
