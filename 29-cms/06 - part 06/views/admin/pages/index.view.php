e08504219ff1be7f76d495332e1dbe7217be3360
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2025-09-20 22:39:04 +0530

    Section - 31 | Done | CMS

---
+++ 29-cms/06 - part 06/views/admin/pages/index.view.php
@@ -0,0 +1,23 @@
+<h1>Admin: Manage Pages</h1>
+
+
+<table style="min-width: 100%;">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php foreach ($pages as $page): ?>
+            <tr>
+                <td><?php echo $page->id; ?></td>
+                <td><?php echo $page->title; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+
+</table>
+
+<a href="index.php?route=admin/pages/create">Create page</a>
---
+++ 29-cms/07 - part 07/views/admin/pages/index.view.php
@@ -0,0 +1,39 @@
+<h1>Admin: Manage Pages</h1>
+
+
+<table style="min-width: 100%;">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php foreach ($pages as $page): ?>
+            <tr>
+                <td><?php echo $page->id; ?></td>
+                <td><?php echo $page->title; ?></td>
+                <td>
+
+                    <form method="POST" action="index.php?<?php echo http_build_query(['route' => 'admin/pages/delete']); ?>">
+                        <input type="hidden" name="id" value="<?php echo e($page->id); ?>" />
+                        <input type="submit" value="Delete!" />
+                    </form>
+
+                    <?php /*
+                    <a href="index.php?<?php echo http_build_query(['route' => 'admin/pages/delete','id' => e($page->id)]); ?>">
+                        Delete
+                    </a>
+                    */
+                    ?>
+
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+
+</table>
+
+<a href="index.php?route=admin/pages/create">Create page</a>
